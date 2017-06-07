a857536880884495d71c1c2b4a7bd44dfabb4aaf
Mark Abella <mark.abella@gmail.com>
2017-06-07 12:54:13 -0700

    View list of available chat rooms

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,8 +1,10 @@
 (function() {
-  function HomeCtrl() {
-  }
+  function HomeCtrl($scope, Room) {
 
+  this.rooms = Room.all;
+
+  }
   angular
   .module('blocChat')
-  .controller('HomeCtrl', [HomeCtrl]);
+  .controller('HomeCtrl', ['$scope', 'Room', HomeCtrl]);
 })();
